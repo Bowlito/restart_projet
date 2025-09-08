212fe276a0c4e12bacf3714d5995e4032c40a760
Bowlito <julien.catoio@gmail.com>
2025-09-08 10:18:43 +0200

    test mdp admin

--- db.sql
+++ db.sql
@@ -9,8 +9,17 @@ USE tripster_db;
 CREATE TABLE users (
     id_users INT PRIMARY KEY AUTO_INCREMENT,
     nom VARCHAR(30),
-    prenom VARCHAR (30),
-    email VARCHAR (30),
+    prenom VARCHAR(30),
+    email VARCHAR(30),
     password VARCHAR(255),
     role VARCHAR(10)
-)
+);
+
+INSERT INTO users VALUES(
+        NULL,
+        "Catoio",
+        "Julien",
+        "test@test.fr",
+        "$2b$10$3H71jScFc1he11PSp6Rp7.y2jH3gqfSmJBLxUAJyYIGa1PNHUzDRO",
+        "admin"
+    )
