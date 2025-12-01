36c1dce65d820a0b15cdec51a4876ed58888f37c
Julien <etoile.montante@dignamik.fr>
2025-12-01 12:14:18 +0100

    securite post com

--- routes/publications.route.js
+++ routes/publications.route.js
@@ -2,16 +2,17 @@ import express from 'express'
 import publicationsController from '../controllers/publications.controller.js'
 import commentairesController from '../controllers/commentaires.controller.js'
 import { verifyToken } from '../middlewares/auth.middleware.js'
+import { verifUser } from '../middlewares/verifUser.middleware.js'
 
 const routeur = express.Router()
 
 routeur.get('/', publicationsController.showAll)
 routeur.get('/:id', verifyToken, publicationsController.showOne)
-routeur.get('/count/:id', publicationsController.count)
-routeur.get('/famous/:id', publicationsController.famous)
+routeur.get('/count/:id', verifUser, publicationsController.count)
+routeur.get('/famous/:id', verifUser, publicationsController.famous)
 routeur.get('/:id/commentaires', verifyToken, commentairesController.theseComs)
-routeur.post('/create', verifyToken, publicationsController.create)
-routeur.put('/modify', verifyToken, publicationsController.modify)
-routeur.delete('/:id', verifyToken, publicationsController.remove)
+routeur.post('/create', verifyToken, verifUser, publicationsController.create)
+routeur.put('/modify', verifyToken, verifUser,  publicationsController.modify)
+routeur.delete('/:id', verifyToken,  publicationsController.remove)
 
 export default routeur
